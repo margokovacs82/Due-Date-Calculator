fcf8db4e23fc0612fd44bc2f8517544bf73999fb
margokovacs82 <margo.kovacs@gmail.com>
2018-06-27 13:07:35 +0200

    refactored version 1

--- CalculateDueDate.js
+++ CalculateDueDate.js
@@ -1,22 +1,46 @@
 'use strict';   
 
 function calculateDueDate(submitDate, turnAroundTime) {                    
-  // the function receives a submitdate in timestamp format and a turnaround time in a string
+
+  const HOURS_IN_MILLISECONDS = 1000 * 60 * 60;
   let hoursGivenInTheForm = parseInt(turnAroundTime, 10);                  
-  // parsing the number in the string
-  let dueDateWithoutOvertimeHandling = 0;                             
+  let dueDateBeforeCheck;     
+  let dueDate;                        
   
   let daysToWorkingHours = Math.floor(hoursGivenInTheForm / 8) * 24;        
-  // 8 hours count as a day
   let remainingHours = hoursGivenInTheForm % 8; 
-  let allTheHoursTogether = daysToWorkingHours + remainingHours; 
-  let timeTillDeadLine = allTheHoursTogether * 1000 * 60 * 60;              
-  // getting the time till deadline in timestamp format
+
+  dueDateBeforeCheck = submitDate + (remainingHours * HOURS_IN_MILLISECONDS);
+  let getTheHours = new Date(dueDate).getUTCHours();
+
+  if (getTheHours > 16) {                                                                         
+    let difference = getTheHours - 16;                                                            
+    let endOfTheDayBefore = dueDateBeforeCheck - (difference * HOURS_IN_MILLISECONDS);    
+    let finalDueDate = endOfTheDayBefore + (16 * HOURS_IN_MILLISECONDS) + (difference * HOURS_IN_MILLISECONDS); 
+  }
+  else if (getTheHours === 0) {
+    dueDate = submitDate + (23 * HOURS_IN_MILLISECONDS);
+  }
+  else {
+    dueDate = dueDateBeforeCheck;
+  }
+
+  for (let i=0; i < daysToWorkingHours; i++) {
+    let getTheWeekends = new Date(dueDate).getUTCDay();
+    if (getTheWeekends === 5) {
+      dueDate += (2 * 24 * HOURS_IN_MILLISECONDS);
+    }
+    dueDate += (24 * HOURS_IN_MILLISECONDS);
+  }
+  return new Date(dueDate).toUTCString();
+}
   
-  dueDateWithoutOvertimeHandling = submitDate + timeTillDeadLine;           
+
+/*
+  dueDate = submitDate + timeTillDeadLine;           
   //the time of finishing the task, timestamp format - can fall to overtime and on weekends
    
-  let getTheHours = new Date(dueDateWithoutOvertimeHandling).getUTCHours();                         
+  let getTheHours = new Date(dueDate).getUTCHours();                         
   //what time would the task be finished, in hours
     if (getTheHours > 16) {                                                                         
       // if after 17:00...
@@ -53,6 +77,8 @@ function calculateDueDate(submitDate, turnAroundTime) {
           }
     }
 };
+
+*/
 
 console.log(new Date(1530024580143).toUTCString());    
 // logging out the test case value 
