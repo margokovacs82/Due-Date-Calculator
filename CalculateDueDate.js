ad102490ff182871cc07e732f07625bc554bdb4f
Kovacs <margo.kovacs@capture.eu>
2018-08-30 09:49:20 +0200

    corrections

--- CalculateDueDate.js
+++ CalculateDueDate.js
@@ -6,27 +6,18 @@ function calculateDueDate(submitDate, turnAroundTime) {
   let dueDateBeforeCheck;
   let dueDate;
   
-  let daysToWorkingHours = Math.floor(hoursGivenInTheForm / 8); // 8 hours count as one day
-  let remainingHours = hoursGivenInTheForm % 8;
-
-   // the time of finishing the task, timestamp format (can fall on overtime and on weekends)
+  let daysToWorkingHours = Math.floor(hoursGivenInTheForm / 8); 
   dueDateBeforeCheck = submitDate + (remainingHours * HOURS_IN_MILLISECONDS);
-  // what time would the task be finished, in hours
   let getTheHours = new Date(dueDate).getUTCHours();
 
-  // if the deadline is after 17:00 it gets corrected
   if (getTheHours > 16) {                                                                         
     dueDate = dueDateBeforeCheck + (16 * HOURS_IN_MILLISECONDS); 
-  // if the deadline falls after 24:00 it gets corrected
   } else if (getTheHours === 0) {
     dueDate = submitDate + (23 * HOURS_IN_MILLISECONDS);
-  // if the deadline is before 17:00 it is not corrected
   } else {
     dueDate = dueDateBeforeCheck;
   }
 
-  /* after the finishing hours of the deadline are corrected
-  we iterate through the days to eliminate the weekends */
   for (let i=0; i < daysToWorkingHours; i++) {
     let getTheWeekends = new Date(dueDate).getUTCDay();
     if (getTheWeekends === 5) {
@@ -37,5 +28,5 @@ function calculateDueDate(submitDate, turnAroundTime) {
   return new Date(dueDate).toUTCString();
 }
   
-console.log(new Date(1530092388026).toUTCString()); // logging out the test case value 
-console.log(calculateDueDate(1530092388026, '26h')); // logging out the result of the method
+console.log(new Date(1530092388026).toUTCString()); 
+console.log(calculateDueDate(1530092388026, '26h'));
